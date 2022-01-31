c6845b6f3d3a792adf926e6755e3a8eedc09002e
Naveen G <naveeng2402@gmail.com>
2022-01-31 09:44:20 +0530

    merged changes from master f33d906

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,55 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch } = require("gulp");
 const minifyJs = require("gulp-uglify");
 const concat = require("gulp-concat");
 const babel = require("gulp-babel");
 
 const bundleCore = () => {
-    return src("./src/js/core/**/*.js")
-    .pipe(concat('core.js'))
-    .pipe(babel({
-        presets: ["@babel/preset-env"]
-        }))
+  return src("./src/js/core/**/*.js")
+    .pipe(concat("core.js"))
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
     .pipe(minifyJs())
     .pipe(dest("./website/static/js/"));
-}
+};
 
 const bundleUi = () => {
-    return src("./src/js/ui/**/*.js")
-    .pipe(babel({
-        presets: ["@babel/preset-env"]
-        }))
+  return src("./src/js/ui/**/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
     .pipe(minifyJs())
     .pipe(dest("./website/static/js/ui/"));
+};
+
+const bundleUi_dev = () => {
+  return src("./src/js/ui/**/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(dest("./website/static/js/ui/"));
+};
+
+async function bundleJs() {
+  await bundleCore();
+  await bundleUi();
 }
 
-async function bundleJs () {
-    await bundleCore();
-    await bundleUi();
+async function bundleJs_dev() {
+  await bundleCore();
+  await bundleUi_dev();
 }
 
 const devWatch = () => {
-    watch("./src/js/**/*.js", bundleJs);
-}
+  watch("./src/js/**/*.js", bundleJs_dev);
+};
 
 exports.default = bundleJs;
 exports.watch = devWatch;
+exports.dev = bundleUi_dev;
--- src/js/ui/login/common.js
+++ src/js/ui/login/common.js
@@ -4,6 +4,7 @@
  * @param {SubmitEvent} event
  */
 function submit_form(form, event) {
+  console.log("inside form");
   event.preventDefault();
 
   const btn = form.querySelector("button");
@@ -20,33 +21,49 @@ function submit_form(form, event) {
   const form_value = get_form_values(form);
   console.log(form_value);
 
-  //   const api_data = backend_api("/admin/login-val", form_value);
-  //   api_data.then((data) => {
-  //     console.log(data);
+  const api_data = backend_api("/common/login-val", form_value);
+  api_data.then((data) => {
+    console.log(data);
 
-  //     if (data["status"] == true) {
-  //       //   window.location.href = data["redirect_url"];
-  //       window.location.replace(data["redirect_url"]);
-  //     } else {
-  //       /**@type {HTMLLabelElement} */
-  //       var username_err_lbl = document.getElementById(
-  //         "admin-login-form__username-input-err"
-  //       );
-  //       var password_err_lbl = document.getElementById(
-  //         "admin-login-password-input-err"
-  //       );
+    if (data["status"] == true) {
+      // window.location.href = data["redirect_url"];
+      window.location.replace(data["redirect_url"]);
+    } else {
+      /**@type {HTMLLabelElement} */
+      var username_err_lbl = document.getElementById("form-username-err");
+      var password_err_lbl = document.getElementById("form-password-err");
+      const username_err = data["errors"]["form-username-input-field"];
+      const password_err = data["errors"]["form-password-input-field"];
+      var username_parent_classList = username_err_lbl.parentElement.classList;
+      var password_parent_classList = password_err_lbl.parentElement.classList;
+      if (username_err != "") {
+        username_err_lbl.innerText = username_err;
+        username_err_lbl.style.opacity = 1;
+        username_parent_classList.add("input-text-anim--focus-err");
+        document.getElementById("form-username-input-field").select();
+      } else {
+        if (username_parent_classList.contains("input-text-anim--focus-err")) {
+          username_parent_classList.remove("input-text-anim--focus-err");
+        }
+        username_err_lbl.style.opacity = 0;
+      }
+      if (password_err != "") {
+        password_err_lbl.innerText = password_err;
+        password_err_lbl.style.opacity = 1;
+        password_err_lbl.parentElement.classList.add(
+          "input-pass-anim--focus-err"
+        );
+        document.getElementById("form-password-input-field").select();
+      } else {
+        if (password_parent_classList.contains("input-pass-anim--focus-err")) {
+          password_parent_classList.remove("input-pass-anim--focus-err");
+        }
+        password_err_lbl.style.opacity = 0;
+      }
+    }
 
-  //       username_err_lbl.style.opacity = 1;
-  //       password_err_lbl.style.opacity = 1;
-
-  //       username_err_lbl.innerHTML =
-  //         data["errors"]["admin-login-form__username-input-field"];
-  //       password_err_lbl.innerHTML =
-  //         data["errors"]["admin-login-form__password-input-field"];
-  //     }
-
-  //     btn.className = old_class;
-  //     btn.style.fontSize = old_font_size;
-  //     btn.innerText = old_innertext;
-  //   });
+    btn.classList.toggle("btn--disabled");
+    btn.style.fontSize = old_font_size + "px";
+    btn.innerText = old_innertext;
+  });
 }
